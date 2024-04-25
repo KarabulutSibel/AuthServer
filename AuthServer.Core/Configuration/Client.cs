df1171ffb978ee017011a218e04db929ba9c483c
sibelkrblt <sibel.karabulut.20@gmail.com>
2024-04-25 23:44:10 +0300

    Add project files.

---
+++ AuthServer.Core/Configuration/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Configuration
+{
+	public class Client
+	{
+        public string Id { get; set; }
+        public string Secret { get; set; }
+        public List<string> Audiences { get; set; }
+
+    }
+}
---
+++ AuthServer.Core/Services/IAuthenticationService.cs
@@ -0,0 +1,18 @@
+﻿using AuthServer.Core.Dtos;
+using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Services
+{
+	public interface IAuthenticationService
+	{
+		Task<Response<TokenDto>> CreateTokenAsync(LoginDto loginDto);
+		Task<Response<TokenDto>> CreateTokenByRefreshToken(string refreshToken);
+		Task<Response<NoDataDto>> RevokeRefreshToken(string refreshToken);
+		Task<Response<ClientTokenDto>> CreateTokenByClient(ClientLoginDto clientLoginDto);
+	}
+}
